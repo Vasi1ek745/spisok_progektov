4a9079ba44daf6d897870ff9003f89663e979cbd
Vasi1ek745 <vasi1ek745@gmail.com>
2023-07-06 11:33:35 +0500

    add depended destroy, main page change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     @task.destroy
 
-    redirect_to root_path, status: :see_other
+    redirect_to tasks_path, status: :see_other
 
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-    has_many :points
+    has_many :points, dependent: :destroy
 
 
     validates :title, length: { minimum: 3 }, presence: true, uniqueness: true
